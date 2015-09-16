04a09d7870a32c4a940ba3e46748ec8a64ae77bc
Aleksandr Ivanov <ivanov-alex@yandex.ru>
2015-09-16 09:28:29 +0300

    added Google projects to the list

--- checkstyle-tester/my_launch.sh
+++ checkstyle-tester/my_launch.sh
@@ -1,4 +1,7 @@
 ./launch.sh -Dcheckstyle.config.location=check_imports_defaults.xml
 ./launch.sh -Dcheckstyle.config.location=check_AvoidStarImports.xml
 ./launch.sh -Dcheckstyle.config.location=check_CustomImportOrder.xml
+./launch.sh -Dcheckstyle.config.location=check_ImportOrder.xml
+./launch.sh -Dcheckstyle.config.location=check_bothImportOrders.xml
+./launch.sh -Dcheckstyle.config.location=google_checks.xml
 
